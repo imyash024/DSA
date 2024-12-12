8b83cf7725239e15d608899aa941c88882b23a55
imyash024 <yjaiswal593@gmail.com>
2024-12-12 19:33:14 +0530

    Solved the Problem on leetcode and added it to my github

--- binarySearchIn_2d.cpp
+++ binarySearchIn_2d.cpp
@@ -33,6 +33,41 @@ n == matrix[i].length
 -104 <= matrix[i][j], target <= 104
 */
 
+
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int start = 0;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int end = row*col -1;
+        int rowIndex, colIndex;
+        int mid = start + (end-start)/2;
+        while(start <= end)
+        {
+            mid = start +(end-start)/2;
+            rowIndex = (mid/col);
+            colIndex = mid - (col*rowIndex);
+            cout<< matrix[rowIndex][colIndex] <<" ";
+            if(matrix[rowIndex][colIndex] == target )
+            {
+                return 1;
+            }
+            else if(target> matrix[rowIndex][colIndex])
+            {
+                start = mid +1;
+            }
+            else
+            {
+                end = mid -1;
+            }
+        }
+        return 0;
+    }
+};
+
 int main()
 {
     return 0;
