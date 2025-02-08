31684fdc0ecc29e472740fcf0339f6ad5b962f4f
imyash024 <yjaiswal593@gmail.com>
2025-02-08 22:25:10 +0530

    Added Binary Search function

--- binarySearchRecursion.cpp
+++ binarySearchRecursion.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
+bool binarySearch(int*arr, int s, int e, int k)
+{
+    if (s>e)
+    {
+        return false;
+    }
+    
+    int mid = s + ((e - s)/2);
+    if(arr[mid] == k)
+    {
+        return true;
+    }
+    else if (arr[mid] > k)
+    {
+        binarySearch(arr, s, mid-1, k);
+    }
+    else
+    {
+        binarySearch(arr, mid + 1, e, k);
+    }
+    
+}
 
 int main()
 {
+    
     
 }
